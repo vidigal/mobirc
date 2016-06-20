cd08d63057817c20501bdaf3ff6ffe6409691cc4
victorvidigal <victorvidigal@gmail.com>
2016-06-20 13:03:39 -0300

    server.py: responsável por receber as conexões dos clientes e armazenar cada cliente em uma lista. server_configy.py: arquivo com configurações do servidor message_manager.py: responsável pela distribuição das mensagens encaminhadas ao servidor.

---
+++ server/message_manager.py
@@ -0,0 +1,50 @@
+# -*- coding: UTF-8 -*-
+import socket
+from threading import Thread
+import select
+import time
+import ast
+
+
+class MessageManager(Thread):
+
+    def __init__(self, sock):
+        Thread.__init__(self)
+        self.sock = sock
+        self.messages = []
+
+    def run(self):
+        while True:
+            self.range_clients()
+            self.dispatch_messages()
+            time.sleep(5)  # Tirar isso depois
+
+    def get_client_sockets(self):  # Tenho que usar esse método para passar uma lista de sockets para o select
+        client_sockets = []
+        for client_socket in self.sock.clients:
+            client_sockets.append(client_socket[0][0])
+        return client_sockets
+
+    def range_clients(self):
+        if self.sock.clients:
+            receive_ready, send_ready, except_ready = select.select(self.get_client_sockets(), [], [], 0.01)
+            # Receber mensagens
+            for client in receive_ready:
+                print('incluindo')
+                self.messages.append(client.recv(512).decode())
+
+    def dispatch_messages(self):
+        while self.messages:
+            msg = ast.literal_eval(self.messages.pop())
+            print(msg)
+            to = self.get_client_sock_by_id(msg['to'])
+            print('to', to)
+            if to is not None:
+                print('Enviando mensagem "%s" de %s para %s' % (msg['msg'], msg['sender'], msg['to']))
+                to.send(msg['msg'].encode())
+
+    def get_client_sock_by_id(self, id):
+        for client_socket in self.sock.clients:
+            print(client_socket)
+            if client_socket[1][0] == id:
+                return client_socket[0][0]
---
+++ server/server.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+import socket
+
+import server_config as config
+from message_manager import MessageManager
+
+
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients = []
+        self.accepting = True
+        self.message_manager = MessageManager(self)
+
+    def connect(self):
+        print('Starting up on %s port %s' % (self.host, self.port))
+        self.sock.bind((self.host, self.port))
+        self.sock.listen(1)  # Put socket into server mode
+        while self.accepting:
+            client_connected = self.sock.accept()
+            print('Connection from', client_connected)
+            client_identify = client_connected[0].recv(64).decode()  # Isso depois pode virar uma função de autenticação
+            self.clients.append((client_connected, client_identify))
+
+    def start_message_manager(self):
+        self.message_manager.start()
+
+    def close(self):
+        print('Closing server socket')
+        self.accepting = False
+        self.sock.close()
+
+
+
+# Criar objeto da classe server e startar servidor
+server = Server(config.address['host'], config.address['port'])
+server.start_message_manager()
+server.connect()
+
+server.close()
+
+#Está faltando fechar o connection
